d122ddfad30748c2b0703fc69053f1ccfc7c3aa2
Kyle Poole <kyle@collectiveintellect.com>
2011-11-09 11:35:42 -0700

    switched to rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tspoon::Application.config.session_store :cookie_store, key: '_tspoon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tspoon::Application.config.session_store :active_record_store
