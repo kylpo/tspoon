a3999f9777748e0d5f418dc4157e984395994749
Kyle Poole <kyle@collectiveintellect.com>
2011-11-09 11:35:42 -0700

    Initial commit on layout branch. Added rspec and a pages controller.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tspoon::Application.config.secret_token = 'e284346b9c0936bc1803a4639d3e0209e29cbe9c2658eda45362030a76786b702d187fa5304b2f6d896dbeada9ed29259544b0be27eacd17caa7d794df98fa2d'
