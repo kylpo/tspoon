aa5fbdc1dd25ef141d2cc54dc95d04253eececa1
Kyle Poole <kyle@collectiveintellect.com>
2011-11-18 13:27:19 -0700

    changes to uses pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Tspoon::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  get "pages/home"
+
+  root :to => "pages#home"
   resources :users
   resources :sessions
-  get "pages/home"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
