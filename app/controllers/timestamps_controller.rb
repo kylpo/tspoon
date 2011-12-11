04276cdeb3e42e81bc07a43a05ae39e2192aa28a
Kyle Poole <kyle@collectiveintellect.com>
2011-12-11 16:56:47 -0700

    redirecting

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -10,11 +10,13 @@ class TimestampsController < ApplicationController
     if(params.has_key?(:hash_input))
       timestamp = HashTimestamper.new( params[:hash_input] )
       Timestamp.create( :value => timestamp.doTimestamp )
-      render 'index'
+      redirect_to timestamps_path
+      # render 'index'
     elsif (params.has_key?(:file_input))
       timestamp = FileTimestamper.new( params[:file_input] )
       Timestamp.create( :value => timestamp.doTimestamp )
-      render 'index'
+      # render 'index'
+      redirect_to timestamps_path
     else
       raise 'ERROR: Input (file or hash) required'
     end
