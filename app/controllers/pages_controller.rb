b8a42a8c0c9ec542af0ed35c6170fa50b9b1cb35
Alex Beal <alexlbeal@gmail.com>
2011-12-10 17:10:56 -0700

    If you visit the homepage with a mobile browser, you get redirected.
    1) I added a mobileView? method in the pages controller, which redirects
    	you to the mobile view if the hit the homepage with a mobile browser.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
   def home
+		if mobileBrowser?
+			redirect_to m_path
+		end
   end
+
+	private
+
+	def mobileBrowser?
+		if request.user_agent =~ /Mobile|webOS/
+			return true
+		else
+			return false
+		end
+	end
 
 end
