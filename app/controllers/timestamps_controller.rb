df891523fcf967d752aa99984af324a5fdd4cf25
Kyle Poole <kyle@collectiveintellect.com>
2011-12-11 14:06:00 -0700

    ...

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -1,11 +1,7 @@
-require "#{Rails.root}/lib/timestamper/timestamper.rb"
-# require "#{Rails.root}/lib/timestamper/filetimestamper.rb"
-# Dir["#{Rails.root}/lib/timestamper/*.rb"].each {|file| require file }
-
 class TimestampsController < ApplicationController
   def create
-    @timestamp = Timestamper.new(
-    Timestamp.create( :value => @timestamp.doTimestamp )
+    @timestamp = Hashtimestamper.new
+    # Timestamp.create( :value => @timestamp.doTimestamp )
 
   end
 end
