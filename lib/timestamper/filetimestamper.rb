22e07636a25c2e8450501196d225306f536d2d60
Josh Wepman <josh.wepman@gmail.com>
2011-12-11 14:26:35 -0700

    Modified gem to modularize Timestamper

--- lib/timestamper/filetimestamper.rb
+++ lib/timestamper/filetimestamper.rb
@@ -3,30 +3,32 @@ require 'tempfile'
 require 'timestamper'
 require 'openssldispatcher'
 
-class FileTimestamper < Timestamper
+module Timestamper
+	class FileTimestamper < Timestamper
 
-	def getHash()
-		return @hash
-	end
+		def getHash()
+			return @hash
+		end
 
-	def initialize(file, timestampAuthority='http://www.startssl.com/timestamp')
-		@file = file #an already-instantiated tmp file object
-		@timestampAuthority = timestampAuthority
-	end
-	
-	def doTimestamp()
-		#generate tmp file for tsq, tsr
-		tsqTmpFile = Tempfile.new("TS_tsq")
-		tsrTmpFile = Tempfile.new("TS_tsr")
+		def initialize(file, timestampAuthority='http://www.startssl.com/timestamp')
+			@file = file #an already-instantiated tmp file object
+			@timestampAuthority = timestampAuthority
+		end
 		
-		OpensslDispatcher.doCreateTSQFromTmpFile(@file, tsqTmpFile)
-		OpensslDispatcher.doCreateTSRFromTSQ(@timestampAuthority, tsqTmpFile, tsrTmpFile)
-		tsrTmpFile.rewind
-		retVal = tsrTmpFile.read
-		tsqTmpFile.close
-		tsrTmpFile.close
-		tsqTmpFile.unlink
-		tsrTmpFile.unlink
-		return retVal	
+		def doTimestamp()
+			#generate tmp file for tsq, tsr
+			tsqTmpFile = Tempfile.new("TS_tsq")
+			tsrTmpFile = Tempfile.new("TS_tsr")
+			
+			OpensslDispatcher.doCreateTSQFromTmpFile(@file, tsqTmpFile)
+			OpensslDispatcher.doCreateTSRFromTSQ(@timestampAuthority, tsqTmpFile, tsrTmpFile)
+			tsrTmpFile.rewind
+			retVal = tsrTmpFile.read
+			tsqTmpFile.close
+			tsrTmpFile.close
+			tsqTmpFile.unlink
+			tsrTmpFile.unlink
+			return retVal	
+		end
 	end
 end
--- lib/timestamper/timestamper.rb
+++ lib/timestamper/timestamper.rb
@@ -1,12 +1,14 @@
 #JDW - Nov 17 2011 - Timestamper Abstract Class (use concrete classes that subtype this for impl)
 
-class Timestamper
-	def initialize()
-		raise 'ERROR: Cant instantiate abstract timestamper class :('
-	end
+module Timestamper
+	class Timestamper
+		def initialize()
+			raise 'ERROR: Cant instantiate abstract timestamper class :('
+		end
 	
-	def doTimestamp()
-		raise 'ERROR: Cant call doTimestamp on abstract timestamper class :('
-	end
+		def doTimestamp()
+			raise 'ERROR: Cant call doTimestamp on abstract timestamper class :('
+		end
 
+	end
 end
