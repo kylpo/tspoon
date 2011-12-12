8aa79586b9279ae3c4f87e91e72e8f82221b9b7b
Kyle Poole <kyle@collectiveintellect.com>
2011-12-11 17:05:38 -0700

    using @timestamps

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -7,16 +7,16 @@ class TimestampsController < ApplicationController
   end
 
   def create
-    if(params.has_key?(:hash_input))
-      timestamp = HashTimestamper.new( params[:hash_input] )
-      Timestamp.create( :value => timestamp.doTimestamp )
-      redirect_to timestamps_path
-      # render 'index'
-    elsif (params.has_key?(:file_input))
+    if (params.has_key?(:file_input))
       timestamp = FileTimestamper.new( params[:file_input] )
       Timestamp.create( :value => timestamp.doTimestamp )
       # render 'index'
       redirect_to timestamps_path
+    elsif(params.has_key?(:hash_input))
+      timestamp = HashTimestamper.new( params[:hash_input] )
+      Timestamp.create( :value => timestamp.doTimestamp )
+      redirect_to timestamps_path
+      # render 'index'
     else
       raise 'ERROR: Input (file or hash) required'
     end
